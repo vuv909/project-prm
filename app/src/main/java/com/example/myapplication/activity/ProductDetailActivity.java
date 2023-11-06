87e4bfdf972a7f339c2648d2b10309f666932646
vu minh tu <vuv909@gmail.com>
2023-11-07 02:43:25 +0700

    fix add to cart buy

--- app/src/main/java/com/example/myapplication/activity/ProductDetailActivity.java
+++ app/src/main/java/com/example/myapplication/activity/ProductDetailActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication.activity;
 import com.example.myapplication.R;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,7 @@ import com.example.myapplication.models.Product;
 import com.example.myapplication.models.User;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
 
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -24,10 +27,10 @@ import java.util.Locale;
 public class ProductDetailActivity extends AppCompatActivity {
 
 
-
+    Gson gson;
     ImageView dtImg;
     TextView name,desc,price;
-    Button addtocart,buy;
+    Button addtocart,buy,soldOut;
     ImageView plus , minus;
 
     Product newProduct = null;
@@ -69,12 +72,14 @@ public class ProductDetailActivity extends AppCompatActivity {
         Avai = findViewById(R.id.available);
         Het = findViewById(R.id.hethang);
         numberQuantity = findViewById(R.id.numberQuantity);
+        soldOut = findViewById(R.id.sold_out);
 
         //product detail
         NumberFormat vndFormat = NumberFormat.getCurrencyInstance(new Locale("vi", "VN"));
         plus.setOnClickListener(this::handlePlus);
         minus.setOnClickListener(this::handleMinus);
         addtocart.setOnClickListener(this::handleAddToCart);
+        buy.setOnClickListener(this::handleBuy);
 
         if (newProduct != null) {
             productInDetail=newProduct;
@@ -86,6 +91,9 @@ public class ProductDetailActivity extends AppCompatActivity {
                 Avai.setVisibility(View.VISIBLE);
             }else if(newProduct.getQuantity() == 0){
                 Het.setVisibility(View.VISIBLE);
+                soldOut.setVisibility(View.VISIBLE);
+                addtocart.setVisibility(View.INVISIBLE);
+                buy.setVisibility(View.INVISIBLE);
             }
             numberQuantity.setText(String.valueOf("Còn "+newProduct.getQuantity()+" sản phẩm trong cửa hàng"));
         }
@@ -100,6 +108,9 @@ public class ProductDetailActivity extends AppCompatActivity {
                 Avai.setVisibility(View.VISIBLE);
             }else if(productModel.getQuantity() == 0){
                 Het.setVisibility(View.VISIBLE);
+                soldOut.setVisibility(View.VISIBLE);
+                addtocart.setVisibility(View.INVISIBLE);
+                buy.setVisibility(View.INVISIBLE);
             }
             numberQuantity.setText(String.valueOf("Còn "+productModel.getQuantity()+" sản phẩm trong cửa hàng"));
         }
@@ -114,8 +125,44 @@ public class ProductDetailActivity extends AppCompatActivity {
                 Avai.setVisibility(View.VISIBLE);
             }else if(showAllModel.getQuantity() == 0){
                 Het.setVisibility(View.VISIBLE);
+                soldOut.setVisibility(View.VISIBLE);
+                addtocart.setVisibility(View.INVISIBLE);
+                buy.setVisibility(View.INVISIBLE);
             }
             numberQuantity.setText(String.valueOf("Còn "+showAllModel.getQuantity()+" sản phẩm trong cửa hàng"));
+        }
+    }
+
+    private void handleBuy(View view) {
+        String quantity = "1";
+        String product_id = String.valueOf(productInDetail.getId());
+        String image_product = String.valueOf(productInDetail.getImg());
+        String price = String.valueOf(productInDetail.getPrice());
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            CollectionReference cartRef = db.collection("Carts");
+
+
+
+        gson = new Gson();
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String userData = sharedPreferences.getString("user", null);
+
+        if (userData != null) {
+
+            User user = gson.fromJson(userData, User.class);
+            CartItem cartItem = new CartItem(user.getId(), Integer.valueOf(product_id), productInDetail.getName(), image_product, Integer.valueOf(price), Integer.valueOf(quantity));
+            cartRef.add(cartItem)
+                    .addOnSuccessListener(documentReference -> {
+                        // Successfully added to the cart
+                        Toast.makeText(this, "Successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ProductDetailActivity.this,ViewCartActivity.class));
+                    })
+                    .addOnFailureListener(e -> {
+                        // Failed to add to the cart
+                        Toast.makeText(this, "Failed to buy", Toast.LENGTH_SHORT).show();
+                    });
+        } else {
+            Toast.makeText(this, "Failed !!!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -124,24 +171,38 @@ public class ProductDetailActivity extends AppCompatActivity {
 
         String quantity = editQuantity.getText().toString(); // Make sure editQuantity is initialized correctly
         String product_id = String.valueOf(productInDetail.getId());
+        String product_name = String.valueOf(productInDetail.getName());
         String image_product = String.valueOf(productInDetail.getImg());
         String price = String.valueOf(productInDetail.getPrice());
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference cartRef = db.collection("Carts");
+        if(Integer.parseInt(quantity) > productInDetail.getQuantity()){
+            Toast.makeText(this, "OOPS!!! Cửa hàng chúng tôi không đủ sản phẩm cho bạn !!!", Toast.LENGTH_SHORT).show();
+        }else {
 
-        CartItem cartItem = new CartItem(1, Integer.valueOf(product_id), image_product, Integer.valueOf(price), Integer.valueOf(quantity));
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            CollectionReference cartRef = db.collection("Carts");
 
-        // Add the cart item to the "Carts" collection
-        cartRef.add(cartItem)
-                .addOnSuccessListener(documentReference -> {
-                    // Successfully added to the cart
-                    Toast.makeText(this, "Added to cart successfully", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(e -> {
-                    // Failed to add to the cart
-                    Toast.makeText(this, "Failed to add to cart", Toast.LENGTH_SHORT).show();
-                });
+            gson = new Gson();
+            SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+            String userData = sharedPreferences.getString("user", null);
+
+            if (userData != null) {
+
+                User user = gson.fromJson(userData, User.class);
+                CartItem cartItem = new CartItem(user.getId(), Integer.valueOf(product_id), productInDetail.getName(), image_product, Integer.valueOf(price), Integer.valueOf(quantity));
+
+                // Add the cart item to the "Carts" collection
+                cartRef.add(cartItem)
+                        .addOnSuccessListener(documentReference -> {
+                            // Successfully added to the cart
+                            Toast.makeText(this, "Added to cart successfully", Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnFailureListener(e -> {
+                            // Failed to add to the cart
+                            Toast.makeText(this, "Failed to add to cart", Toast.LENGTH_SHORT).show();
+                        });
+            }
+        }
     }
 
 
